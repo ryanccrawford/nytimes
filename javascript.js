d4a5a5ed9d7b87e6fe7c22f7477addd843fdf36d
RyBo <ryanccrawford@live.com>
2019-04-13 12:09:31 -0400

    a

--- javascript.js
+++ javascript.js
@@ -15,19 +15,43 @@ $(document).ready(function () {
            // + filter
             + "&"
             + apikey
-
+        var topArticles = $('#topArticles')
         $.ajax({
             url: endpoint,
             method: "GET"
         }).then(function (response) {
-            
+           
             var retrieved = response.response
+            console.log(retrieved)
+            
+            console.log(retrieved.docs)
+            var articlesLength = retrieved.docs.length
+            for (let i = 0; i < articlesLength; i++){
+                var articles = retrieved.docs[i],
 
+                    headline = articles.headline.main,
+                    snippit = articles.snippit,
+                    date = articles.pub_date,
+                    link = articles.web_url
+                
+                var headlineH1 = $('<h1>').text(headline).addClass('')
+                var dateP = $('<p>').text(date).addClass('')
+                var snippitP = $('<p>').text(snippit).addClass('')
+                var linkA = $('<a>').attr('href', link)
+                $(linkA).append(headlineH1)
+                var articleBlock = $('<div>').addClass('articleBlock')
+
+                $(articleBlock).append(linkA).append(dateP).append(snippitP)
+
+                topArticles.prepend(articleBlock)
+            }
+            
 
 
         })
 })
 
+    
     
 
 
