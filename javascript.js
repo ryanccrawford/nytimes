8c15da9260756246edaa58158ace0937479ea366
RyBo <ryanccrawford@live.com>
2019-04-13 11:41:52 -0400

    java

--- javascript.js
+++ javascript.js
@@ -1,2 +1,36 @@
-var apikey = 'uobpTwgmKD1c2H2c4iMrUuBJG6zbPDUv';
+$(document).ready(function () { 
 
+
+    
+    
+    $('#submit').click(function(){
+    
+        
+        var apikey = '&api-key=uobpTwgmKD1c2H2c4iMrUuBJG6zbPDUv';
+        var query = 'SpaceX' //$('#articleBox').val().trim();
+       // var filter = //$('#filter').val()
+        var endpoint = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' +
+            query //+
+           // '&fq='
+           // + filter
+            + "&"
+            + apikey
+
+        $.ajax({
+            url: endpoint,
+            method: "GET"
+        }).then(function (response) {
+            
+            var retrieved = response.response
+
+
+
+        })
+})
+
+    
+
+
+
+
+})
