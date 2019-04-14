2331de36d53eb1005e9adae7f8f035fdb0dffbd0
RyBo <ryanccrawford@live.com>
2019-04-14 09:31:05 -0400

    fixed and cleaned up backend

--- javascript.js
+++ javascript.js
@@ -1,36 +1,44 @@
 $(document).ready(function () { 
 
 
+    var endpoint = 'https://api.nytimes.com/svc/search/v2/articlesearch.json'
     
-    
-    $('#submit').click(function(){
-    
+    $('#clear').click(function () {
+        $(input).val('')
+    })
+    $('#submit').click(function(event){
+        event.preventDefault()
         
-        var apikey = '&api-key=uobpTwgmKD1c2H2c4iMrUuBJG6zbPDUv';
-        var query = $('#articleBox').val().trim();
+        var searchFor = $('#articleBox').val().trim()
       
-        var endpoint = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' +
-            query 
-            + "&"
-            + apikey
+        //TODO: Grab other search filters to the query, like by date or author
+        var query = {
+                          'api-key': 'uobpTwgmKD1c2H2c4iMrUuBJG6zbPDUv',
+                                'a': searchFor
+                    }
+
         var topArticles = $('#topArticles')
+        
         $.ajax({
             url: endpoint,
-            method: "GET"
+            method: "GET",
+            data: data
         }).then(function (response) {
            
+
             var retrieved = response.response
-            console.log(retrieved)
-            
-            console.log(retrieved.docs)
+        
             var articlesLength = retrieved.docs.length
+
+            //loop through the objects array
             for (let i = 0; i < articlesLength; i++){
                 var articles = retrieved.docs[i],
 
                     headline = articles.headline.main,
-                    snippit = articles.snippit,
+                    snippit = articles.snippit ? articles.snippit:'', // checks if snippit exist
                     date = articles.pub_date,
                     link = articles.web_url
+                
                 
                 var headlineH1 = $('<h1>').text(headline).addClass('')
                 var dateP = $('<p>').text(date).addClass('')
@@ -48,9 +56,6 @@ $(document).ready(function () {
 
         })
 })
-
-    
-    
 
 
 
